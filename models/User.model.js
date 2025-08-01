e00591362eeea6acb38efbf85518156ab3ed43b0
prabodhkumar005 <prabodhkumar7254@gmail.com>
2025-08-01 23:24:06 +0530

    created user schema

---
+++ models/User.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    bio:{
+        type:String,
+        default:""
+    },
+    occupation: {
+        type: String,
+    },
+    photoUrl:{
+        type:String,
+        default:""
+    },
+    linkdin:{
+        type:String,
+        default:""
+    }
+
+},{timestamps:true})
+
+export const User=mongoose.model("User",userSchema);
